b47b78173cb64050fab8cd365c89482c2d794cd0
ykolluru <141166270+ykolluru@users.noreply.github.com>
2023-08-01 14:14:30 -0400

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ streamlit.text(' 🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
